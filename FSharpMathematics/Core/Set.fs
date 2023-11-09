e167436640f0848f224c84f7f93cb46716b95f7b
Thobias Larsen <static.thobias@outlook.com>
2023-11-09 20:49:13 +0100

    Added Bernoulli distribution to F# implementation

---
+++ FSharpMathematics/Core/Set.fs
@@ -0,0 +1,9 @@
+﻿module Set
+
+type Set = array<obj>
+
+let create_set(size: int) : Set =
+    Array.zeroCreate size
+
+let copy_set(set: Set) : Set =
+    Array.copy set
---
+++ FSharpMathematics/Probability/Distributions/Bernoulli.fs
@@ -0,0 +1,14 @@
+﻿module Bernoulli
+
+open System.Collections.Generic
+
+type BernoulliDist = Dictionary<int, float>
+
+let create_distribution(p) : BernoulliDist =
+    let dist = new Dictionary<int, float>()
+    dist.Add(1, p)
+    dist.Add(0, float 1 - p)
+    dist
+
+let bernoulli(dist: BernoulliDist, v) =
+    dist.Item v
