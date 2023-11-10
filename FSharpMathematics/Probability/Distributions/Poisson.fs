18195f38d7a251a5de5eb7002a4894a19a1b1b9c
StaTicDKK17 <tobse2002@gmail.com>
2023-11-10 21:31:54 +0100

    Added Poisson distribution

---
+++ FSharpMathematics/Probability/Distributions/Poisson.fs
@@ -0,0 +1,23 @@
+﻿module Poisson
+
+open System.Collections.Generic
+open MathFunctions
+
+type PoissonDist = Dictionary<int, float>
+
+let mutable saved_dist = new PoissonDist()
+
+let create_distribution(lambda: float) (lim) =
+    let dist = new PoissonDist()
+    for i in 0..lim do
+        dist.Add(i, (exp (-lambda) * (lambda ** i))/ float (factorial i))
+    dist
+
+let evaluate(k) =
+    saved_dist.Item k
+
+let evaluate_distribution(dist: PoissonDist) (k) =
+    dist.Item k
+
+let save_distribution(dist: PoissonDist) =
+    saved_dist <- dist
